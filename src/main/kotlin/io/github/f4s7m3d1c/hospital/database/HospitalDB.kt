725865529685143aa1207c3930812df2f9d4460d
found-cake <skymin468@gmail.com>
2023-08-07 23:09:54 +0900

    HospitalDB

--- src/main/kotlin/io/github/f4s7m3d1c/hospital/database/HospitalDB.kt
+++ src/main/kotlin/io/github/f4s7m3d1c/hospital/database/HospitalDB.kt
@@ -1,5 +1,6 @@
 package io.github.f4s7m3d1c.hospital.database
 
+import io.github.f4s7m3d1c.hospital.hospital.Hospital
 import java.sql.Connection
 import java.sql.Statement
 
@@ -12,9 +13,34 @@ class HospitalDB(conn: Connection) {
 
 		fun init(conn: Connection){
 			val statement: Statement = conn.createStatement()
-
+			statement.execute("CREATE TABLE IF NOT EXISTS `hospital_info_db` (\n" +
+					"    `version` INT UNSIGNED NOT NULL,\n" +
+					"    `name` VARCHAR(50) NOT NULL,\n" +
+					"    `latitude` DECIMAL(20, 16) NOT NULL ,\n" +
+					"    `longitude` DECIMAL(20, 16) NOT NULL ,\n" +
+					"    `hasER` TINYINT(1) NOT NULL,\n" +
+					"    `timeMon` VARCHAR(20) NOT NULL,\n" +
+					"    `timeTue` VARCHAR(20) NOT NULL,\n" +
+					"    `timeWen` VARCHAR(20) NOT NULL,\n" +
+					"    `timeThu` VARCHAR(20) NOT NULL,\n" +
+					"    `timeFri` VARCHAR(20) NOT NULL,\n" +
+					"    `timeSat` VARCHAR(20) NOT NULL,\n" +
+					"    `timeSun` VARCHAR(20) NOT NULL\n" +
+					")")
 			statement.close()
 			instance = HospitalDB(conn)
 		}
+	}
+
+	fun removeVersion(version: UInt) {
+		//TODO
+	}
+
+	fun getHospitals(latitude: Double, longitude: Double) {
+		//TODO
+	}
+
+	fun addHospital(hospital: Hospital) {
+		//TODO
 	}
 }
