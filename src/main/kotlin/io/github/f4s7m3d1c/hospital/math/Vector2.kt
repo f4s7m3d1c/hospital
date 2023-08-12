a3916c9113853fe04ba7f7df554b81be2a5d6e04
found-cake <skymin468@gmail.com>
2023-08-13 00:34:22 +0900

    Vector2

--- src/main/kotlin/io/github/f4s7m3d1c/hospital/math/Vector2.kt
+++ src/main/kotlin/io/github/f4s7m3d1c/hospital/math/Vector2.kt
@@ -1,12 +1,40 @@
 package io.github.f4s7m3d1c.hospital.math
 
-class Vector2(
-	val latitude: Double,
-	val longitude: Double
-) {
+import kotlin.math.*
+import java.lang.Math.toRadians
+import java.lang.Math.toDegrees
+
+private const val EARTH_RADIUS = 6371.0 // 지구 반지름
+
+class Vector2(val latitude: Double, val longitude: Double) {
 
 	fun distance(vector: Vector2): Double {
-		// TODO: 위도 경도를 가지고 km 거리 계산
-		return 0.0
+		val lat1Rad: Double = toRadians(latitude)
+		val lon1Rad: Double = toRadians(longitude)
+		val lat2Rad: Double = toRadians(vector.latitude)
+		val lon2Rad: Double = toRadians(vector.longitude)
+
+		val latDiff: Double = (lat2Rad - lat1Rad) / 2
+		val lonDiff: Double = (lon2Rad - lon1Rad) / 2
+
+		val a: Double = sin(latDiff) * sin(latDiff) + cos(lat1Rad) * cos(lat2Rad) * sin(lonDiff) * sin(lonDiff)
+		val c: Double = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+		return EARTH_RADIUS * c
+	}
+
+	fun calculateBoundingBox(sideLength: Double = 2.0): Pair<Vector2, Vector2> {
+		val latDiff: Double = toDegrees(sideLength / (2 * EARTH_RADIUS))
+		val lonDiff: Double = toDegrees(sideLength / (2 * EARTH_RADIUS * cos(toRadians(latitude))))
+
+		val minLat: Double = latitude - latDiff
+		val maxLat: Double = latitude + latDiff
+		val minLon: Double = longitude - lonDiff
+		val maxLon: Double = longitude + lonDiff
+
+		return Pair(
+			Vector2(minLat, minLon),
+			Vector2(maxLat, maxLon)
+		)
 	}
 }
