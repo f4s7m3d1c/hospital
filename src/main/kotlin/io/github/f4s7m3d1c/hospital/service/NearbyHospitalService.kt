38144948a76ebd14b6a4029917db9b2f03c433f2
found-cake <skymin468@gmail.com>
2023-08-22 10:12:27 +0900

    logic error

--- src/main/kotlin/io/github/f4s7m3d1c/hospital/service/NearbyHospitalService.kt
+++ src/main/kotlin/io/github/f4s7m3d1c/hospital/service/NearbyHospitalService.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service
 class NearbyHospitalService {
 
 	fun getNearbyHospital(pos: Vector2): MutableMap<String, Any> {
-		val hospitals: MutableList<Hospital> = HospitalDB.INSTANCE.getHospitals(pos.calculateBoundingBox(1.5))
+		val hospitals: MutableList<Hospital> = HospitalDB.INSTANCE.getHospitals(pos.calculateBoundingBox(5.0))
 		val nearbyHospitals: MutableList<Hospital> = mutableListOf()
 		hospitals.forEach {
 			if (it.position.distance(pos) <= 1.3) {
